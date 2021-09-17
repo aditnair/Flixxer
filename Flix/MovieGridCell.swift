7f6956e342edaf5f4621059729ed2d763511f69b
aditnair <adityanair@ufl.edu>
2021-09-16 22:30:19 -0400

    Flix - Part 2

---
+++ Flix/MovieGridCell.swift
@@ -0,0 +1,16 @@
+//
+//  MovieGridCell.swift
+//  Flix
+//
+//  Created by Aditya Nair on 9/16/21.
+//
+
+import UIKit
+
+class MovieGridCell: UICollectionViewCell {
+    
+    @IBOutlet weak var posterView: UIImageView!
+    
+    
+    
+}
